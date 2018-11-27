76e1f823e46c64022ba5d5b0af6f2664f46cad27
JBelic <42744843+JBelic@users.noreply.github.com>
2018-11-27 13:51:47 +0100

    Add files via upload

--- addlig.py
+++ addlig.py
@@ -13,105 +13,40 @@ start = time.time()
 
 # Path to the working folder where are prepared molecules and where folder with new coordinares
 # will be made with the specific name
-
-working_folder_path = "/Users/jelena/Desktop/Adding-ligands-master"
+working_folder_path = os.getcwd()
 
 input_ligands = read_molecules(os.path.join(working_folder_path,'LIGANDS'))
-input_cores = read_molecules(os.path.join(working_folder_path,'CORES'))
+input_ligands = [bob(input_ligands[ligand]) for ligand in input_ligands]
+input_cores = read_molecules(os.path.join(working_folder_path,'benz'))
+input_cores = [bob(input_cores[core]) for core in input_cores]
+
 
 new_directory = 'new_molecules'
-distance_limit_squared = 2.89 # square of 1.7; if atoms are closer, geometry is rejected
+distance_limit = 1.7 # if atoms are closer, geometry is rejected
 
 
-##############################################          new folders              ############################################
+##############################################          new folder               ############################################
 
-# Arranging folders
-#working_folder = os.path.join(working_folder_path,new_directory)
-#folders = ["monosubstituted", "disupstituted", "error_folder"]
-#for i in range(len(folders)):
+# Makes new folder
 if not os.path.exists(os.path.join(working_folder_path,new_directory)):
     os.makedirs(os.path.join(working_folder_path,new_directory))
 
-##############################################         Monosubstitution      ################################################
+############################################ Monosubstitution and disubstitution ############################################
 
-##### Preparing coordinates #####
-
-#Extracting core coordinates from imported dictionary
-coords_c = mol_coords(input_cores)
-#Extracting ligand coordinates from imported dictionary
-ligand_coords = mol_coords(input_ligands)
-#Making list of lists with core coordinates. Core molecules are copied as many times as there are ligands.
-cores_list = []
-for i in range(len(input_ligands)):
-    kopi = copy.deepcopy(coords_c)
-    cores_list.append(kopi)
-    
-#Exctracting core and ligand names from imported dictionary
-lig_names = mol_names(input_ligands)
-cor_names = mol_names(input_cores)
-
-##### Monosubstitution #####
-
-# To every list of cores one type of ligand is added
-# mono_subs contaions of key = name of molecule, value = (coordinates of new molecule, shortest distance between core and ligand after its connection)
-mono_subs = {}
-for d in range(len(input_ligands)):
-    # In each list c goes through all cores. New copies of ligands are needed in every loop
-    for c in range(len(input_cores)): 
-       	ligando = copy.deepcopy(ligand_coords)
-       	mono_subs[cor_names[c]+ "_" + lig_names[d]] = (connect_two_molecules(cores_list[d][c],ligando[d],distance_limit_squared))
-
-#For diatomic molecules, distance is specified by user and it's given as a string. 
-#For the other molecule it's given as a float and it serves like indicator for steric clash, if distance is too small, molecule is stored in separate folder (list of folders made on the beggining) 
-
-for name, molecule in mono_subs.items():
-    if molecule[1] == True:
-        molecule[0].write(os.path.join(working_folder_path,new_directory, 'mono_' + name +'.xyz'))
-    else:
-        molecule[0].write(os.path.join(working_folder_path,new_directory, 'err_' + name + '.xyz'))
-        print ("Molecule %s has problematic geometry!" % name)
+new_molecules = mono_di_substitution(input_ligands, input_cores, distance_limit)
+# mono_subs contains sublists with plams molecule ('molecule') and binary ('check')
+# depending on binary value, False or True, name of the coordinate file is with or without 'err'.
+for item in new_molecules: 
+    print (new_molecules.index(item))
+    n= ['mono','di']
+    for molecule, check in item:
+        if check == True:
+            molecule.write(os.path.join(working_folder_path,new_directory, n[new_molecules.index(item)] + '_' + molecule.properties.name +'.xyz'))
+        else:
+            molecule.write(os.path.join(working_folder_path,new_directory, 'err_' + n[new_molecules.index(item)]+ molecule.properties.name + '.xyz'))
 
 
-
-
-
-##############################################         Disubstitution      ################################################
-
-##### Preparing coordinates #####
-
-# Copies of coordinates of monosupstituted molecules   
-monosub_coords = [x[0] for x in mol_coords(mono_subs)]
-# Making list of lists with monosubs coordinates. Core molecules are copied as many times as there are ligands.
-monosub_copies = []
-for i in range(len(input_ligands)):
-    kopi = copy.deepcopy(monosub_coords)
-    monosub_copies.append(kopi)
-
-ligand_coords2 = mol_coords(input_ligands)
-monosub_names = mol_names(mono_subs)
-
-####### Disustitution #####
-
-# Takes copies of monosubstituted cores and again adds ligands
-di_subs = {}
-for d in range(len(input_ligands)):
-    for c in range(d*len(input_cores),len(mono_subs)):
-        ligando = copy.deepcopy(ligand_coords2) 
-        di_subs [monosub_names[c]+ "_" + lig_names[d]] = (connect_two_molecules(monosub_copies[d][c],ligando[d],distance_limit_squared))
-
-#For diatomic molecules, distance is specified by user and it's given as a string. 
-#For the other molecule it's given as a float and it serves like indicator for steric clash, if distance (float number) is too small molecule is stored in separate folder 
-
-
-for name, molecule in di_subs.items():
-    if molecule[1] == True:
-        molecule[0].write(os.path.join(working_folder_path,new_directory, 'di_' + name +'.xyz'))
-    else:
-        molecule[0].write(os.path.join(working_folder_path,new_directory, 'err_' + name + '.xyz'))
-        print ("Molecule %s has problematic geometry!" % name)       
-
-
-# The End 
+# The End
 end = time.time()
 print("Elapsed wall-clock time:  ", end - start)
 
